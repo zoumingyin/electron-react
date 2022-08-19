902388249025be7a07460afb64e465c05c61b8ef
zoumingyin <mingyin.zou@heweisoft.com>
2022-08-19 17:33:15 +0800

    'init'

--- .history/src/App_20220819165720.js
+++ .history/src/App_20220819165720.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  ddd多大多大
+          Learn React
         </a>
       </header>
     </div>
